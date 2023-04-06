8302edcea6aebb81bfefe2643aba88f7f5d1c740
Joonseon-Kwak12 <kjoonseon12@gmail.com>
2023-04-06 15:52:15 +0900

    서블릿->JSP 파일 요청사항 넘겨줘서 협업 가능

--- src/main/java/com/KoreaIT/java/jam/HomeMainServlet3.java
+++ src/main/java/com/KoreaIT/java/jam/HomeMainServlet3.java
@@ -8,10 +8,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 @WebServlet("/printDan")
 public class HomeMainServlet3 extends HttpServlet {
-    public HomeMainServlet3() {
-        super();
-    }
-
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     	response.setContentType("text/html; charset=UTF-8");
     	
